96b0f329b39e8145555b07736d3f5513fb0a81f1
sqc <sqc157400661@163.com>
2020-09-27 08:30:49 +0800

    1

--- test.go
+++ test.go
@@ -2,43 +2,20 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"runtime"
 	"time"
 )
 
-func showNumber (i int) {
-	defer fmt.Println(1111)
-	//runtime.Goexit()
-	os.Exit(1)
-	fmt.Println(i)
-}
-
+var x,y int
 func main() {
-
-	for i := 0; i < 10; i++ {
-		go showNumber(i)
-	}
-
-	runtime.Gosched()
-	fmt.Println("Haha")
-}
-
-func longRunning(messages <-chan string) {
-	timer := time.NewTimer(time.Minute)
-	defer timer.Stop()
-	for {
-		select {
-		case <-timer.C: // 过期了
-			return
-		case msg := <-messages:
-			fmt.Println(msg)
-			// 此if代码块很重要。
-			if !timer.Stop() {
-				<-timer.C
-			}
+	go func(){
+		x=1
+		y=1
+	}()
+	go func(){
+		if y==1 {
+			h := y/x
+			fmt.Println(h)
 		}
-		// 必须重置以复用。
-		timer.Reset(time.Minute)
-	}
+	}()
+time.Sleep(time.Second)
 }
