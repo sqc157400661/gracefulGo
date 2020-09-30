f819b8539783dae5d7c5344daedc396d977060a3
sqc <sqc157400661@163.com>
2020-09-30 08:42:14 +0800

    1

--- test.go
+++ test.go
@@ -1,21 +1,11 @@
 package main
-
-import (
-	"fmt"
-	"time"
-)
-
-var x,y int
+import "fmt"
 func main() {
-	go func(){
-		x=1
-		y=1
-	}()
-	go func(){
-		if y==1 {
-			h := y/x
-			fmt.Println(h)
-		}
-	}()
-time.Sleep(time.Second)
+	// 假设此切片的长度很大，以至于它的元素
+	// 将被开辟在堆上。
+	bs := make([]byte, 1 << 31)
+	// 一个聪明的编译器将觉察到bs的底层元素
+	// 部分已经不会再被使用，而正确地认为bs的
+	// 底层元素部分在此刻可以被安全地回收了。
+	fmt.Println(len(bs))
 }
