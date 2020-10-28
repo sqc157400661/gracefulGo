f70116eec95c581d3da562feb033732e26e4679a
sqc <sqc157400661@163.com>
2020-10-28 18:35:51 +0800

    通道知识汇总1

--- test.go
+++ test.go
@@ -1,21 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-	"unsafe"
-)
-
-type Person struct{
-	sex int
-	age int
-}
 func main() {
-	t := Person{}
-	fmt.Println(unsafe.Alignof(t))
-	fmt.Println(unsafe.Alignof(t.age))
-	fmt.Println(unsafe.Alignof(t.sex))
-
-	fmt.Println(reflect.TypeOf(t).Align())
-	fmt.Println(reflect.TypeOf(t).FieldAlign())
+	//go func() {
+	//	for  {
+	//		fmt.Println(11)
+	//	}
+	//}()
+	//var ball = make(chan string)
+	//kickBall := func(playerName string) {
+	//	for {
+	//		fmt.Print(<-ball, "传球", "\n")
+	//		time.Sleep(time.Second)
+	//		ball <- playerName
+	//	}
+	//}
+	//go kickBall("张三")
+	//go kickBall("李四")
+	//go kickBall("王二麻子")
+	//go kickBall("刘大")
+	//ball <- "裁判"   // 开球
+	var c chan bool // 一个零值nil数据通道
+	<-c             // 永久阻塞在此
 }
