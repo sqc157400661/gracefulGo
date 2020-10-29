de75cd9e6193aa212ef1edf1ddec84943d1f7aad
sqc <sqc157400661@163.com>
2020-10-29 11:15:36 +0800

    数据通道和通道用例

--- test.go
+++ test.go
@@ -1,24 +1,6 @@
 package main
 
+
 func main() {
-	//go func() {
-	//	for  {
-	//		fmt.Println(11)
-	//	}
-	//}()
-	//var ball = make(chan string)
-	//kickBall := func(playerName string) {
-	//	for {
-	//		fmt.Print(<-ball, "传球", "\n")
-	//		time.Sleep(time.Second)
-	//		ball <- playerName
-	//	}
-	//}
-	//go kickBall("张三")
-	//go kickBall("李四")
-	//go kickBall("王二麻子")
-	//go kickBall("刘大")
-	//ball <- "裁判"   // 开球
-	var c chan bool // 一个零值nil数据通道
-	<-c             // 永久阻塞在此
+	select{}
 }
