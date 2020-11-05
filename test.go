7ddfe378631e400e94a811a6ee115a4f6269ac4f
sqc <sqc157400661@163.com>
2020-11-05 18:56:41 +0800

    1

--- test.go
+++ test.go
@@ -1,6 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	select{}
+	c := make(chan string, 2)
+	trySend := func(v string) {
+		select {
+		case c <- v:
+		}
+	}
+	tryReceive := func()  {
+		select {
+		case v := <-c: fmt.Println(v)
+
+		}
+	}
+	go trySend("Hello!") // 发送成功
+	go trySend("Hi!")    // 发送成功
+	go trySend("Bye!")   // 发送失败，但不会阻塞。
+	for i:=0; i<=2;i++  {
+		go tryReceive()
+	}
+	time.Sleep(time.Second*3)
 }
