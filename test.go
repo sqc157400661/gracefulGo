08cfb705ec4319430e277f142026300cb2fb8962
sqc <sqc157400661@163.com>
2020-11-14 09:12:57 +0800

    1

--- test.go
+++ test.go
@@ -2,27 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"reflect"
 )
 
-func main() {
-	c := make(chan string, 2)
-	trySend := func(v string) {
-		select {
-		case c <- v:
-		}
-	}
-	tryReceive := func()  {
-		select {
-		case v := <-c: fmt.Println(v)
+type User struct {
+	Name string
+}
 
-		}
+func main() {
+	i := 1
+	fmt.Println("i type name", reflect.TypeOf(i).Name())
+	fmt.Println("i type kind", reflect.TypeOf(i).Kind())
+
+	u := User{Name: "poloxue"}
+	fmt.Println("u type name", reflect.TypeOf(u).Name())
+	fmt.Println("u type kind", reflect.TypeOf(u).Kind())
+
+	if reflect.TypeOf(u).Kind() == reflect.Struct {
+		fmt.Println("u kind is struct")
 	}
-	go trySend("Hello!") // 发送成功
-	go trySend("Hi!")    // 发送成功
-	go trySend("Bye!")   // 发送失败，但不会阻塞。
-	for i:=0; i<=2;i++  {
-		go tryReceive()
-	}
-	time.Sleep(time.Second*3)
+ 
 }
