8fa23c7c3b0c8748e17f6ec22d0829c6be300a47
sqc <sqc157400661@163.com>
2020-11-28 14:40:50 +0800

    测试

--- test.go
+++ test.go
@@ -1,17 +1,66 @@
 package main
 
-import "runtime"
+//Operator 是被封装的实际类接口
+type Operator interface {
+	SetA(int)
+	SetB(int)
+	Result() int
+}
 
-func DoSomething() {
-	for {
-		// 做点什么...
+//OperatorFactory 是工厂接口
+type OperatorFactory interface {
+	Create() Operator
+}
 
-		runtime.Gosched() // 防止本协程霸占CPU不放
+//OperatorBase 是Operator 接口实现的基类，封装公用方法
+type OperatorBase struct {
+	a, b int
+}
+
+//SetA 设置 A
+func (o *OperatorBase) SetA(a int) {
+	o.a = a
+}
+
+//SetB 设置 B
+func (o *OperatorBase) SetB(b int) {
+	o.b = b
+}
+
+//PlusOperatorFactory 是 PlusOperator 的工厂类
+type PlusOperatorFactory struct{}
+
+func (PlusOperatorFactory) Create() Operator {
+	return &PlusOperator{
+		OperatorBase: &OperatorBase{},
 	}
 }
 
-func main() {
-	go DoSomething()
-	go DoSomething()
-	select{}
+//PlusOperator Operator 的实际加法实现
+type PlusOperator struct {
+	*OperatorBase
+}
+
+//Result 获取结果
+func (o PlusOperator) Result() int {
+	return o.a + o.b
+}
+
+//MinusOperatorFactory 是 MinusOperator 的工厂类
+type MinusOperatorFactory struct{}
+
+func (MinusOperatorFactory) Create() Operator {
+	return &MinusOperator{
+		OperatorBase: &OperatorBase{},
+	}
+}
+
+//MinusOperator Operator 的实际减法实现
+type MinusOperator struct {
+	*OperatorBase
+}
+
+//Result 获取结果
+func (o MinusOperator) Result() int {
+	return o.a - o.b
 }
